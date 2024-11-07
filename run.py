c2a00f6477d57a6ab5bb7e80c937a74b3113b759
koles <kolesnikov.pavel.sergeevich@gmail.com>
2024-11-07 21:53:17 +0700

    Простой эхо-бот на команду help

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from config import TOKEN
@@ -13,8 +13,13 @@ dp = Dispatcher()
 async def cmd_start(message: Message):
     await message.answer('Привет')
 
+@dp.message(Command('help'))  # Бот ждет ввода команды help и запускает функцию ниже.
+async def get_help(message: Message):  # На вход получает команду от хендлера "help" и выводит ответ ниже
+    await message.answer('Это команда /help')  # Просто ответ в чате на наш запрос - отвечает 'Это команда /help'
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
